3b902829ce616d33abec7f5708c07c5c426a5981
Stanislav <stanislavasilevich@gmail.com>
2024-11-18 16:30:33 +0300

    выполнил часть задач первого урока понедельника

--- 1.1/index.js
+++ 1.1/index.js
@@ -18,3 +18,52 @@ const user = {name: 'Bob'};
 const user2 = user;
 user2.name = "Alex";
 console.log(user.name);
+
+const arr = [1, 2, 3, 4];
+const arr2 = [6, 7, 8];
+arr.push(5);
+console.log(arr);
+console.log(arr2);
+const arr3 = arr.concat(arr2);
+console.log('arr3:', arr3);
+console.log(arr3 === arr); // false
+
+const users = [
+  {
+    id: 1,
+    name: "Bob",
+    isStudent: true,
+  },
+  {
+    id: 2,
+    name: "Alex",
+    isStudent: true,
+  },
+  {
+    id: 3,
+    name: "Ann",
+    isStudent: true,
+  },
+  {
+    id: 4,
+    name: "Donald",
+    isStudent: true,
+  },
+]
+
+const newUser = {
+  id: 5,
+  name: "Farid",
+  isStudents: true
+}
+
+// CRUD
+const copyUsers = [
+  users[0],
+  users[2],
+  users[3],
+  users[4],
+  newUser
+]
+
+console.log(users === copyUsers); // false
