40fd6c01210a8e361badfcd9168d77feeceeefb7
Stanislav <stanislavasilevich@gmail.com>
2024-11-18 15:29:57 +0300

    create new project

---
+++ 1.1/index.js
@@ -0,0 +1,14 @@
+/*
+Примитивы
+1. string => "", '', ``
+2. number => number, NaN, Infinity
+3. undefined => undefined
+4. null => null
+5. boolean => true, false
+7. symbol => Symbol
+8. bigint => BigInt
+
+Объекты:
+  - составной (комбинированный)
+9. object => object, array, function
+*/
