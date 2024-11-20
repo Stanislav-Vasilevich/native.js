f9a8923029204cac8e91c9d39d1aa79332fcafe3
Stanislav <stanislavasilevich@gmail.com>
2024-11-20 12:30:53 +0300

    отработал принцип удаления и клонирования объектов

--- 1.1/index.js
+++ 1.1/index.js
@@ -58,6 +58,8 @@ const newUser = {
 }
 
 // CRUD
+// Create
+console.time('one');
 const copyUsers = [
   // users[0],
   // users[2],
@@ -66,5 +68,57 @@ const copyUsers = [
   ...users,
   newUser
 ]
-
+console.timeEnd('one');
+console.time('test');
+const copyUsers2 = users.concat(newUser);
+console.log('copyUsers2: ', copyUsers2);
+console.timeEnd('test');
+// Read
 console.log(users === copyUsers); // false
+
+// Update
+const updateUsers = users.map(i => i.id === 2 ? {...i, isStudent: false} : i);
+
+console.log('users: ', users);
+console.log('updateUsers: ', updateUsers);
+console.log('update: ', updateUsers[0] === users[0]); // true
+console.log('update2: ', updateUsers[1] === users[1]); // false
+
+// Delete
+const userDelete = users.filter(i => i.id !== 1);
+
+console.log('userDelete: ', userDelete);
+console.log('delete: ', users[0] === userDelete[0]); // false
+
+const superUser = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": 'Kulas Light',
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+};
+
+const superUserCopy = {...superUser, company: {...superUser.company, catchPhrase: "harness"}};
+console.log('superUserCopy: ', superUserCopy);
+
+const superUserCopy2 = {...superUser, address: {...superUser.address, geo: {...superUser.address.geo, lat: "55.55", lng: "66.66"}}}
+console.log('superUserCopy2: ', superUserCopy2);
+
+const superUserCopy3 = {...superUser, email: 'stanislavasilevich@yandex.ru'}
+console.log('superUserCopy3: ', superUserCopy3);
