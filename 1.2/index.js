b4d2bf37cf76c9c2d0a64d81df40bf5518792621
Stanislav <stanislavasilevich@gmail.com>
2024-12-13 16:20:17 +0300

    completed the second task on monday

--- 1.2/index.js
+++ 1.2/index.js
@@ -61,12 +61,39 @@ const myPush = (array, elem) => {
   return array.length;
 }
 
+const myIndexOf = (array, el, start = 0) => {
+  for (let i = start; i < array.length; i++) {
+    if(array[i] === el) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+const myInclude = (array, el, start = 0) => {
+  for (let i = start; i < array.length; i++) {
+    if(array[i] === el) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+const array = ['one', 'two', 'three'];
+
 const result1 = myMap(students, st => st.name);
 const result2 = myFilter(students, st => st.name !== 'John');
 const result3 = myFilter(students, st => st.name === 'John');
 const result4 = myPush(students, {name: 'Stanislav', age: 36, isMarried: true, scores: 100});
+const result5 = myIndexOf(array, 'two');
+const result6 = myInclude(array, 'three');
 console.log('result1: ', result1);
 console.log('result2: ', result2);
 console.log('result3: ', result3);
 console.log('result4: ', result4);
 console.log('students: ', students);
+console.log('result5: ', result5);
+console.log('result6: ', result6);
+console.log('proto: ', array.__proto__);
