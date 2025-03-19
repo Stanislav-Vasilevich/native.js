1e2b545d158a19f4d7992786d4b9d51e30ad20e4
Stanislav <stanislavasilevich@gmail.com>
2025-03-19 20:55:05 +0300

    прошел с Виктором 4-е занятие понедельника

--- 1.4/index.js
+++ 1.4/index.js
@@ -1,19 +1,43 @@
-const todolist = [
-  {id: 1, title: 'What to learn', filter: 'all'},
-  {id: 2, title: 'What to buy', filter: 'all'},
+const todolist_1 = 1;
+const todolist_2 = 2;
+
+let todolist = [
+  {id: todolist_1, title: 'What to learn', filter: 'all'},
+  {id: todolist_2, title: 'What to buy', filter: 'all'},
 ]
 
-const tasks = {
-  [todolist[1].id]: [
+let tasks = {
+  [todolist_1]: [
     {id: 1, title: 'HTML', isDone: false},
     {id: 2, title: 'CSS', isDone: false},
     {id: 3, title: 'React', isDone: false}
   ],
-  [todolist[2]]: [
+  [todolist_2]: [
     {id: 1, title: 'Beer', isDone: false},
     {id: 2, title: 'Fish', isDone: false},
     {id: 3, title: 'Water', isDone: false}
   ],
 }
 
-console.log('task1: ', tasks[todolist[1].id]);
+console.log('task1: ', tasks[todolist_1]);
+console.log('task2: ', tasks[todolist_2]);
+
+const createTodolist = (title) => {
+  const todolist_3 = crypto.randomUUID();
+  const newTodolist = {id: todolist_3, title, filter: 'all'};
+  tasks = {...tasks, [todolist_3]: []}
+
+  todolist = [...todolist, newTodolist];
+}
+
+console.log(createTodolist('What to show'));
+
+console.log(todolist);
+console.log(tasks);
+
+const changeTaskStatus = (todolistId, taskId, newStatus) => {
+  tasks = {...tasks, [todolistId]: tasks[todolistId].map(t => t.id === taskId ? {...t, isDone: newStatus} : t)}
+}
+
+changeTaskStatus(todolist_1, 2, true);
+console.log(tasks);
